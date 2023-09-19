40f88121b264bfc6f81e8f56424ade44e6a19578
Sammy Wong <sammywong528@outlook.com>
2023-09-19 15:42:17 +0200

    Init commit, create Person class

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExercise
+{
+    public class Person
+    {
+        private int age;
+        private string fName = String.Empty;
+        private string lName = String.Empty;
+        private int height;
+        private int weight;
+
+        public int Age { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+
+    }
+}
